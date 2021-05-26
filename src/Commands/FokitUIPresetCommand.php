edf555ad738c7721170f6f4691f4e9fda9f54425
Bonge Ian <nkaranjah@gmail.com>
2021-05-26 17:53:49 +0300

    Added the stubs and preset's artisan command

--- src/Commands/FokitUIPresetCommand.php
+++ src/Commands/FokitUIPresetCommand.php
@@ -3,6 +3,7 @@
 namespace BongeIan\FokitUIPreset\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class FokitUIPresetCommand extends Command
 {
@@ -13,6 +14,8 @@ class FokitUIPresetCommand extends Command
     public function handle()
     {
         $this->publishAssets();
+        $this->updateWebpackUrl();
+        $this->updateRoutes();
 
         $this->comment('Fokit UI is now installed.');
         $this->info('Don\'t forget to run npm i && npm run dev');
@@ -20,6 +23,30 @@ class FokitUIPresetCommand extends Command
 
     protected function publishAssets()
     {
-        $this->callSilent('vendor:publish', ['--tag' => 'fortify-ui-preset-resources', '--force' => true]);
+        $this->callSilent('vendor:publish', ['--tag' => 'layout-component-provider', '--force' => true]);
+
+        $this->callSilent('vendor:publish', ['--tag' => 'fokit-ui-preset-resources', '--force' => true]);
+
+        File::deleteDirectory(resource_path('css'));
+    }
+
+    protected function updateWebpackUrl()
+    {
+        File::put(
+            base_path('webpack.mix.js'),
+            str_replace(
+                'http://CHANGE_ME.test',
+                env('APP_URL'),
+                File::get(base_path('webpack.mix.js'))
+            )
+        );
+    }
+
+    protected function updateRoutes()
+    {
+        File::append(
+            base_path('routes/web.php'),
+            "\nRoute::prefix('user')->middleware(['auth', 'verified'])->group(function () {\n\tRoute::view('profile', 'profile.show');\n});\n"
+        );
     }
 }
--- src/FokitUIPresetServiceProvider.php
+++ src/FokitUIPresetServiceProvider.php
@@ -12,8 +12,17 @@ class FokitUIPresetServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../stubs/resources/views' => base_path('resources/views'),
-                // Add more resources here
-            ], 'fortify-ui-preset-resources');
+                __DIR__ . '/../stubs/resources/scss' => base_path('resources/scss'),
+                __DIR__ . '/../stubs/resources/js' => base_path('resources/js'),
+                __DIR__ . '/../stubs/.editorconfig' => base_path('.editorconfig'),
+                __DIR__ . '/../stubs/.gitignore' => base_path('.gitignore'),
+                __DIR__ . '/../stubs/package.json' => base_path('package.json'),
+                __DIR__ . '/../stubs/webpack.mix.js' => base_path('webpack.mix.js'),
+            ], 'fokit-ui-preset-resources');
+
+            $this->publishes([
+                __DIR__ . '/../app/Providers' => base_path('app/Providers'),
+            ], 'layout-component-provider');
 
             $this->commands([
                 FokitUIPresetCommand::class,
