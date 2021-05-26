ef09efa7b61751f7a85831e2f50e3756a5132e7d
Bonge Ian <nkaranjah@gmail.com>
2021-05-26 17:56:19 +0300

    Added dashboard view and profile views

--- stubs/resources/views/dashboard.blade.php
+++ stubs/resources/views/dashboard.blade.php
@@ -1,31 +1,25 @@
-@extends('layouts.app')
-
-@section('content')
-    @if (session('status'))
-        <div>{{ session('status') }}</div>
-    @endif
-
-    <div>You are logged in!</div>
-
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-
-        <button type="submit">
-            {{ __('Logout') }}
-        </button>
-    </form>
-
-    <hr>
-
-    @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updateProfileInformation()))
-        @include('profile.update-profile-information-form')
-    @endif
-
-    @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-        @include('profile.update-password-form')
-    @endif
-
-    @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::twoFactorAuthentication()))
-        @include('profile.two-factor-authentication-form')
-    @endif
-@endsection
+<x-app-layout title="{{ auth()->user()->name }}'s Dashoard">
+	<section class="uk-section" uk-height-viewport="offset-top: true">
+		<div class="uk-container">
+			<div class="uk-flex-center uk-grid" uk-grid>
+				<div class="uk-width-1-1 uk-width-3-5@s">
+					<div class="uk-card uk-box-shadow-small uk-box-shadow-hover-large">
+						<div class="uk-card-body">
+							<h2 class="uk-card-title uk-margin-remove-bottom uk-text-lead uk-text-center">
+								<a href="" class="uk-icon-button uk-button-primary" uk-icon="star"></a>
+								Welcome, {{ auth()->user()->name }}!
+							</h2>
+							<hr>
+							<p class="uk-text-meta uk-text-center uk-margin-remove-top">
+								It's
+								<time datetime="{{ now()->format('c') }}">
+									{{ now()->format('l, F \\t\\h\\e jS \\@ g:i a') }}!
+								</time>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</x-app-layout>
--- stubs/resources/views/profile/update-profile-information-form.blade.php
+++ stubs/resources/views/profile/update-profile-information-form.blade.php
@@ -1,22 +1,42 @@
-<form method="POST" action="{{ route('user-profile-information.update') }}">
-    @csrf
-    @method('PUT')
+<x-profile-settings-card>
+	<x-slot name="title">{{ __('Profile Information') }}</x-slot>
+	<x-slot name="message">{{ __('Update your account\'s profile information and email address.') }}</x-slot>
 
-    <div>
-        <label>{{ __('Name') }}</label>
-        <input type="text" name="name" value="{{ old('name') ?? auth()->user()->name }}" required autofocus autocomplete="name" />
-    </div>
+	<form method="POST" action="{{ route('user-profile-information.update') }}" id="profile-update-info">
+		@csrf
+		@method('PUT')
 
-    <div>
-        <label>{{ __('Email') }}</label>
-        <input type="email" name="email" value="{{ old('email') ?? auth()->user()->email }}" required autofocus />
-    </div>
+		<div class="uk-margin uk-width-large">
+			<label class="uk-form-label">{{ __('Name') }}</label>
 
-    <div>
-        <button type="submit">
-            {{ __('Update Profile') }}
-        </button>
-    </div>
-</form>
+			<div class="uk-width-1-1">
+				<div class="uk-inline uk-width-1-1">
+					<span class="uk-form-icon" uk-icon="icon: user"></span>
+					<input class="uk-input @error('name', 'updateProfileInformation') uk-form-danger @enderror" type="text" name="name" value="{{ old('name') ?? auth()->user()->name }}" required autofocus autocomplete="name" />
+				</div>
 
-<hr>
+				@error('name', 'updateProfileInformation')
+					<x-alert type="danger" :message="$message" />
+				@enderror
+			</div>
+		</div>
+
+
+		<div class="uk-margin uk-width-large">
+			<label class="uk-form-label">{{ __('Email') }}</label>
+
+			<div class="uk-width-1-1">
+				<div class="uk-inline uk-width-1-1">
+					<span class="uk-form-icon" uk-icon="icon: mail"></span>
+					<input class="uk-input @error('email', 'updateProfileInformation') uk-form-danger @enderror" type="email" name="email" value="{{ old('email') ?? auth()->user()->email }}" required />
+				</div>
+				@error('email', 'updateProfileInformation')
+					<x-alert type="danger" :message="$message" />
+				@enderror
+			</div>
+		</div>
+	</form>
+
+	<x-slot name="submit_form_id">{{ _('profile-update-info') }}</x-slot>
+	<x-slot name="cta">{{ _('Save') }}</x-slot>
+</x-profile-settings-card>
