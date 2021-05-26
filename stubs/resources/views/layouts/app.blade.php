4dbff89bb7eb9af701bbf77575335586b725fe53
Bonge Ian <nkaranjah@gmail.com>
2021-05-26 17:52:23 +0300

    Added Layout components

--- stubs/resources/views/layouts/app.blade.php
+++ stubs/resources/views/layouts/app.blade.php
@@ -1,15 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+		<!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-    </head>
-    <body>
-        @yield('content')
-    </body>
+		@isset($title)
+			<title>{{ config('app.name', 'Laravel') }} - {{ $title }}</title>
+		@else
+			<title>{{ config('app.name', 'Laravel') }}</title>
+		@endisset
+
+		<!-- Scripts -->
+		<script src="{{ mix('js/manifest.js') }}"></script>
+		<script src="{{ mix('js/vendor.js') }}"></script>
+
+		<!-- Fonts -->
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Assistant:wght@400;500;700&display=swap" rel="stylesheet">
+
+		<!-- Styles -->
+		<link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+	</head>
+
+	<body>
+		<div id="app">
+			<x-navbar />
+
+			<main class="uk-background-muted">
+
+				<aside class="uk-container">
+	                <div class="uk-margin-top">
+	                    <x-status-alert />
+	                </div>
+	            </aside>
+
+				 {{ $slot }}
+			</main>
+		</div>
+
+		<!-- App -->
+		<script src="{{ mix('js/app.js') }}"></script>
+	</body>
 </html>
